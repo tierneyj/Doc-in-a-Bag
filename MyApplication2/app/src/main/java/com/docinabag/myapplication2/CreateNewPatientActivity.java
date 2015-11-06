7e726b2c5c68bbe00b3705e69306f808cb10b1c2
tierneyj <tierneyj@bu.edu>
2015-11-05 19:39:21 -0500

    Added Activities

--- MyApplication2/app/src/main/java/com/docinabag/myapplication2/CreateNewPatientActivity.java
+++ MyApplication2/app/src/main/java/com/docinabag/myapplication2/CreateNewPatientActivity.java
@@ -1,13 +1,32 @@
 package com.docinabag.myapplication2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateNewPatientActivity extends AppCompatActivity {
+
+    private static Button create_sbm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_patient);
+        createButtonListener();
+    }
+
+    public void createButtonListener() {
+        create_sbm = (Button)findViewById(R.id.createButton);
+        create_sbm.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.docinabag.myapplication2.SelectTestActivity");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
---
+++ MyApplication2/app/src/main/java/com/docinabag/myapplication2/GlucoseTestActivity.java
@@ -0,0 +1,13 @@
+package com.docinabag.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GlucoseTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glucose_test);
+    }
+}
