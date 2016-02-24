461de4a085822a3f6e33b191b4f1f6b020060b8a
tierneyj <tierneyj@bu.edu>
2016-02-24 17:36:33 -0500

    API GET
    now works with new server

--- MyApplication2/app/src/main/java/com/docinabag/myapplication2/SearchForPaitentActivity.java
+++ MyApplication2/app/src/main/java/com/docinabag/myapplication2/SearchForPaitentActivity.java
@@ -26,9 +26,34 @@ public class SearchForPaitentActivity extends AppCompatActivity {
 
     private EditText uid_input;
 
+    private TextView pubpid;
+    private TextView date;
+    private TextView fname;
+    private TextView dob;
+    private TextView sex;
+    private TextView status;
+    private TextView email;
+    private TextView street;
+    private TextView postal_code;
+    private TextView city;
+    private TextView state;
+    private TextView country_code;
+    private TextView phone_contact;
+    private TextView phone_cell;
+    private TextView ss;
+
     private String uid;
 
-    public patientContainer newPatient;
+    private View mInitialView;
+    private ContactInfo record;
+    private View mRecordView;
+    private boolean mVisible;
+    private Button submitButton;
+
+
+    //public patientContainer newPatient;
+
+    public ContactInfo newPatient;
 
     public Button button;
 
@@ -43,6 +68,9 @@ public class SearchForPaitentActivity extends AppCompatActivity {
         button = (Button)findViewById(R.id.button3);
         patientLabel = (TextView)findViewById(R.id.patientFound);
         button.setVisibility(View.GONE);
+        mInitialView = findViewById(R.id.initialView);
+        mRecordView = findViewById(R.id.recordView);
+        mRecordView.setVisibility(View.GONE);
         searchButtonListener();
     }
 
@@ -54,6 +82,7 @@ public class SearchForPaitentActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         uid = uid_input.getText().toString();
+                        newPatient = null;
                         new HttpRequestTask().execute();
 
                     }
@@ -61,30 +90,62 @@ public class SearchForPaitentActivity extends AppCompatActivity {
         );
     }
 
-    private class HttpRequestTask extends AsyncTask<Void, Void, patientContainer> {
+    private void showRecord () {
+        mRecordView.setVisibility(View.VISIBLE);
+        mInitialView.setVisibility(View.GONE);
+         //pubpid = (TextView)findViewById(R.id.
+         //date = (TextView)findViewById(R.id.
+         fname = (TextView)findViewById(R.id.textView_firstName);
+         dob = (TextView)findViewById(R.id.textViewDOB);
+         sex = (TextView)findViewById(R.id.textViewGender);
+         status = (TextView)findViewById(R.id.textViewStatus);
+         email = (TextView)findViewById(R.id.textViewEmail);
+//         street = (TextView)findViewById(R.id.
+//         postal_code = (TextView)findViewById(R.id.
+//         city = (TextView)findViewById(R.id.
+//         state = (TextView)findViewById(R.id.
+//         country_code = (TextView)findViewById(R.id.
+//         phone_contact = (TextView)findViewById(R.id.
+//         phone_cell = (TextView)findViewById(R.id.
+//         ss = (TextView)findViewById(R.id.
+        fname.setText(newPatient.getFname() + " " + newPatient.getLname());
+        dob.setText(newPatient.getDob());
+        sex.setText(newPatient.getSex());
+        status.setText(newPatient.getStatus());
+        email.setText(newPatient.getEmail());
+
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, ContactInfo> {
         @Override
-        protected patientContainer doInBackground(Void... params) {
+        protected ContactInfo doInBackground(Void... params) {
             try {
-                final String url = ("http://52.34.73.235:5000/openemr/api/v0.0/patients/" + uid);
+                final String url = ("http://52.36.163.49:8000/api/patient-data/" + uid +".json/");
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-                newPatient = restTemplate.getForObject(url, patientContainer.class);
+                //newPatient = restTemplate.getForObject(url, patientContainer.class);
+                newPatient = restTemplate.getForObject(url, ContactInfo.class);
+
                 return newPatient;
             } catch (Exception e) {
-                Log.e("MainActivity", e.getMessage(), e);
+                Log.e("Spring Problem", e.getMessage(), e);
             }
 
             return null;
         }
 
         @Override
-        protected void onPostExecute(patientContainer newPatient) {
-            button.setVisibility(View.VISIBLE);
-            patientLabel.setText("Patient Found");
-            button.setText("Begin Testing For " + newPatient.getPatient().getFirstName() + " " + newPatient.getPatient().getLastName());
-            newPatient.getPatient().setMonth((newPatient.getPatient().getDob()).substring(0,2));
-            newPatient.getPatient().setDay((newPatient.getPatient().getDob()).substring(3,5));
-            newPatient.getPatient().setYear((newPatient.getPatient().getDob()).substring(6,10));
+        protected void onPostExecute(ContactInfo newPatient) {
+            showRecord();
+            try {
+                GMailSender sender = new GMailSender("noreply@remotehcs.com", "seniorDesign15");
+                sender.sendMail("This is Subject",
+                        "This is Body",
+                        "noreply@remotehcs.com",
+                        "jht3394@yahoo.com");
+            } catch (Exception e) {
+                Log.e("SendMail", e.getMessage(), e);
+            }
             buttonButtonListener();
         }
 
@@ -93,11 +154,15 @@ public class SearchForPaitentActivity extends AppCompatActivity {
                     new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            Intent intent = new Intent("com.docinabag.myapplication2.SelectTestActivity");
-                            Bundle bundle = new Bundle();
-                            bundle.putSerializable(SER_KEY, newPatient.getPatient());
-                            intent.putExtras(bundle);
-                            startActivity(intent);
+                            try {
+                                GMailSender sender = new GMailSender("noreply@remotehcs.com", "seniordesign15");
+                                sender.sendMail("This is Subject",
+                                        "This is Body",
+                                        "noreply@remotehcs.com",
+                                        "jht3394@yahoo.com");
+                            } catch (Exception e) {
+                                Log.e("SendMail", e.getMessage(), e);
+                            }
 
                         }
                     }
